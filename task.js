29544bc1fd79a9311013f48e1a0be22d1c9aa428
Ann <ania.somko@gmail.com>
2021-11-29 23:39:21 +0200

    changed tasks 2-4

--- task.js
+++ task.js
@@ -10,29 +10,40 @@ function getNum(...args) {
 getNum(1,3,6,8,9);
 
 
-
 /* 2. Вводиш N чисел, програма має вивести тіки парні числа. */
 function even(...args) {
-return args.filter(num => num%2 == 0);
+/*return args.filter(num => num%2 == 0);*/
+let arr = [];
+for(let arg of args) {
+  if(arg%2 == 0) arr.push(arg);
 }
-getNum(4,6,9,10);
+return arr;
+}
+even(4,6,9,10);
 
 /* 3. Вводиш N чисел, програма має вивести суму чисел */
-function sum(...args) {
-   return args.reduce((a,b) => a + b,0);
+function countSum(...args) {
+   /*return args.reduce((a,b) => a + b,0);*/
+   let sum = 0;
+   for(let arg of args) sum += arg;
+   return sum;
 }
-
-sum(2,5,6);
+countSum(2,5,6);
 
 /* 4. Вводиш N чисел, програма має вивести суму чисел, які більше за 5 */
-function sum(...args) {
-    let num = args.reduce((a,b) => a + b,0);
-    return num > 5?num:false;
+function fifthSum(...args) {
+    /*let num = args.reduce((a,b) => a + b,0);*/
+    let sum = 0;
+    for(let arg of args){
+     if(arg > 5) sum += arg;
+    } 
+    return sum;
 }
-sum(2,5,6);
+fifthSum(2,5,6,9);
 
-/* 5. Вводиш координати точок прямокутника на двовимірній площині. І координати довільних точок.
- Виводити інфу, чи ці точки  є в середині прямокутника */
-function triangle() {
-    
+
+/* 6. Вводиш координати точок прямокутника на двовимірній площині. І координати довільних точок.
+ Виводити інфу, чи ці точки  є всередині прямокутника */
+function rectangle() {
+   
 }
